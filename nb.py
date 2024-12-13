ebbd2488ab6fd5a06e775a0b0381e5a1a2da4f8d
nabooll <nabilahmadfawzy@gmail.com>
2024-12-13 16:42:35 +0800

    first commit

--- nb.py
+++
@@ -1,9 +0,0 @@
-n = int(input(""))  # Read the first number which tells how many numbers will follow
-
-total_sum = 0  # Initialize the sum to 0
-
-for _ in range(n):  # Loop through the next 'n' numbers
-    num = int(input(""))  # Read each number
-    total_sum += num  # Add it to the total sum
-
-print(total_sum)  # Print the sum of the numbers
